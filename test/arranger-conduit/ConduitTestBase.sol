5cfc869ccd1dbdfd0d4b5b054a489e9cf054d453
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-08-31 14:58:51 +0900

    fix: update to latest allocators

--- test/arranger-conduit/ConduitTestBase.sol
+++ test/arranger-conduit/ConduitTestBase.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.8.13;
 
 import "dss-test/DssTest.sol";
 
-// TODO: Remove src
-import { AllocatorRegistry } from "dss-allocator/src/AllocatorRegistry.sol";
-import { AllocatorRoles }    from "dss-allocator/src/AllocatorRoles.sol";
+import { AllocatorRegistry } from "dss-allocator/AllocatorRegistry.sol";
+import { AllocatorRoles }    from "dss-allocator/AllocatorRoles.sol";
 
 import { MockERC20 } from "erc20-helpers/MockERC20.sol";
 
